86623d49e7d634231437dee2ebd62ccf7eca2266
Mickaël Derriey <mderriey@gmail.com>
2021-02-17 14:22:07 +1100

    Refactor Blob people generator into a different class

---
+++ src/AzureIdentityLivestream.PeopleGenerator/BlobPeopleGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureIdentityLivestream.PeopleGenerator
+{
+    public static class BlobPeopleGenerator
+    {
+        public static async Task CreatePeople(IConfiguration configuration, IReadOnlyList<Person> people)
+        {
+            var blobServiceClient = new BlobServiceClient(configuration.GetValue<string>("StorageConnectionString"));
+
+            var containerClient = blobServiceClient.GetBlobContainerClient("people");
+            await containerClient.CreateIfNotExistsAsync();
+
+            foreach (var person in people)
+            {
+                var blobName = $"{Guid.NewGuid()}.json";
+                var blobClient = containerClient.GetBlobClient(blobName);
+
+                await using var jsonStream = new MemoryStream();
+                await JsonSerializer.SerializeAsync(jsonStream, person);
+
+                jsonStream.Position = 0;
+                await blobClient.UploadAsync(
+                    jsonStream,
+                    new BlobUploadOptions
+                    {
+                        HttpHeaders = new BlobHttpHeaders
+                        {
+                            ContentType = "application/json"
+                        }
+                    });
+            }
+        }
+    }
+}
--- src/AzureIdentityLivestream.PeopleGenerator/Program.cs
+++ src/AzureIdentityLivestream.PeopleGenerator/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
 namespace AzureIdentityLivestream.PeopleGenerator
@@ -17,31 +12,8 @@ namespace AzureIdentityLivestream.PeopleGenerator
                 .AddUserSecrets<Program>()
                 .Build();
 
-            var blobServiceClient = new BlobServiceClient(configuration.GetValue<string>("StorageConnectionString"));
-
-            var containerClient = blobServiceClient.GetBlobContainerClient("people");
-            await containerClient.CreateIfNotExistsAsync();
-
             var people = await PersonCreator.CreateRandomPeople(30);
-            foreach (var person in people)
-            {
-                var blobName = $"{Guid.NewGuid()}.json";
-                var blobClient = containerClient.GetBlobClient(blobName);
-
-                await using var jsonStream = new MemoryStream();
-                await JsonSerializer.SerializeAsync(jsonStream, person);
-
-                jsonStream.Position = 0;
-                await blobClient.UploadAsync(
-                    jsonStream,
-                    new BlobUploadOptions
-                    {
-                        HttpHeaders = new BlobHttpHeaders
-                        {
-                            ContentType = "application/json"
-                        }
-                    });
-            }
+            await BlobPeopleGenerator.CreatePeople(configuration, people);
         }
     }
 }
